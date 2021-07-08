abb0c4b60799d4eb92efa6e53803ac98105aec95
somiar <somiarzj@hotmail.com>
2021-07-08 18:59:43 +0800

    update translation file

--- src/i18n/es.js
+++ src/i18n/es.js
@@ -10,12 +10,12 @@ export default {
   economicWhitePaper: 'Libro blanco económico',
   technicalWhitePaper: 'Libro blanco técnico',
   startSection: {
-    p1: 'Construye tu propia Economía de Staking',
+    p1: 'Link Community to Web3.0',
     btn1: 'Entrar a Crowdloan',
     btn2: 'Entrar a Peanut'
   },
   factorySection: {
-    title: 'Todo en una misma fabrica DAO',
+    title: 'The DAO Operating System for Web3.0',
     p1: 'Nutbox ofrece las siguientes herramientas a la comunidad',
     t1: 'Fabrica Crowd-Staking',
     desc1: 'Solo toma 5 minutos configurar el modulo de Staking Mining para comenzar una comunidad basada en Economía de Staking.',
